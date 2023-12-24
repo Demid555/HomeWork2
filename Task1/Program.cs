d4817afa1e427a75b75a929ec3a5bc3a115e490d
Demid Kolesnikov <Demidcol@yandex.ru>
2023-12-24 16:23:01 +0300

    Добавил первое задание

---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте двумерный массив символов (тип char [,]). 
+// Создать строку из символов этого массива.
+
+
+string Concat(char[,] array)
+{
+    string str = "";
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++ )
+        {
+            str = str + array[i,j];
+        }
+        
+
+    }
+    return str;
+}
+
+char[,] letters = { {'a', 'b', 'c'}, {'d', 'e', 'f'} };
+string result = Concat(letters);
+
+Console.WriteLine(result);
