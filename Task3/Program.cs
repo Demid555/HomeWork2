d23b624bf55a61d6531c066bf121f9678f8c39fd
Demid Kolesnikov <Demidcol@yandex.ru>
2023-12-24 16:23:36 +0300

    Добавил третье задание

---
+++ Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте произвольную строку. Выясните,
+// является ли она палиндромом.
+
+
+string PalindromStr (string msg)
+
+{
+    bool a = false;
+    string b = "Нет";
+    for (int i = 0; i < msg.Length/2; i++)
+    {
+        if (msg[i] == msg[msg.Length - i - 1])
+        {
+            a = true;
+        } else 
+        {
+            a = false;
+        }
+    }
+    if (a == true)
+    {
+        b = "Да";
+    } else
+    {
+         b = "Нет";
+    }
+    return b;
+}
+
+string str = "556655";
+Console.Write(PalindromStr(str));
